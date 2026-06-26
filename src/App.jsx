04fd12a90e0f69868ccd828b88f9c55e59f6604a
Asif Ali <asephkhan@gmail.com>
2026-06-26 17:33:17 +0100

    started interactive review card

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+
+function App() {
+return (
+  <>
+  <body>
+
+  {/*  Rating state start  */}
+
+  How did we do?
+
+  Please let us know how we did with your support request. All feedback is appreciated 
+  to help us improve our offering!
+
+  1 2 3 4 5
+
+  Submit
+
+  {/*  Rating state end  */}
+
+  {/* <!-- Thank you state start --> */}
+
+  You selected {/* <!-- Add rating here -->  */}out of 5
+
+  Thank you!
+
+  We appreciate you taking the time to give a rating. If you ever need more support, 
+  don’t hesitate to get in touch!
+
+  {/* <!-- Thank you state end --> */}
+
+  
+  <footer class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+    Coded by <a href="#">Your Name Here</a>.
+  </footer>
+</body>
+  </>
+)
+}
+
+export default App
