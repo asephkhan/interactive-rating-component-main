70cdf57dc267005aa11c6f5d48fa24ac164b8a82
Asif Ali <asephkhan@gmail.com>
2026-06-27 16:53:16 +0100

    started making circle components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
 import { useState } from "react";
+import iconStar from "./assets/images/icon-star.svg";
+import "./App.css";
+
+ const Circle = (props) => {
+  return (
+    <>
+    <div className="circle">
+  {props.content}
+    </div>
+    </>
+  )
+} 
 
 const Card = () => {
   return (
     <>
       <div className="card">
+        <img className="card__star_icon" src={iconStar} alt="star icon" />
         <h1>How did we do?</h1>
         <p>
           Please let us know how we did with your support request. All feedback
           is appreciated to help us improve our offering!
         </p>
-        <p>1 2 3 4 5</p>
+        <div className="card__rating_score">
+        <Circle content="1" />
+        <Circle content="2" />
+        <Circle content="3" />
+        <Circle content="4" />
+        <Circle content="5" />
+        </div>
+
+
+        
         <button>Submit</button>
       </div>
     </>
